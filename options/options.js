cb88b55d389473a5a1f6708f64b03c58250d40ff
Crowdbotics <team@crowdbotics.com>
2024-02-03 10:07:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-sea-45815",
+  url: "https://holy-sea-45815.botics.co",
+  api: "https://holy-sea-45815.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
